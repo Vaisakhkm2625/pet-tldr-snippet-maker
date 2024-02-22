cfaed6fe8b4af8205fcfb15f199c7a8c358cf02c
Vaisakh K M <vaisakhkm2625@gmail.com>
2024-02-22 20:34:46 +0530

    fixed "

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import re
-import shutil
 import tempfile
 from typing import List, Optional, Tuple
 
@@ -36,22 +35,44 @@ def scrape_tldr_repo(url: str, category: Optional[str] = None) -> List[Tuple[str
     return result
 
 
+def replace_braces(string):
+    if '{{' in string and '}}' in string:
+        string = string.replace('{{', '<').replace('}}', '>')
+
+    string = string.replace('"', '""')
+
+    return string
+
+
+
 def parse_tldr_folder(category: str, path: str) -> List[Tuple[str, str]]:
     result = []
     for entry in os.listdir(path):
         entry_path = os.path.join(path, entry)
+        print(entry_path)
         if os.path.isfile(entry_path):
             with open(entry_path, "r") as file:
-                result.extend(parse_page(category, file.read()))
+                result.extend(parse_page(file.read()))
     return result
 
 
-def parse_page(category: str, content: str) -> List[Tuple[str, str]]:
-    return [(match.group(1), match.group(2)) for match in PAGES_REGEX.finditer(content)]
+def parse_page(content: str) -> List[Tuple[str, str]]:
+    return [(match.group(1), replace_braces(match.group(2))) for match in PAGES_REGEX.finditer(content)]
 
+def print_toml_snippet(description,cmd):
+    print("[[snippets]]")
+    print(f"  description = \"{description}\"")
+    # Print the command
+    print(f"  command = \"{cmd}\"")
+    # Print the output (if any)
+    print("  output = \"\"")  # You can modify this line if needed
+    # Print a new line to separate each command
+    print()
 
 if __name__ == "__main__":
-    # Example usage:
     commands = scrape_tldr_github("common")
+
     for description, cmd in commands:
-        print(description, "-", cmd)
+        print_toml_snippet(description,cmd)
+
+
